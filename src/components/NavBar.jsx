7d72e2e638aafb1ead70b8d6e5e9d314e3362e95
Sagar <153504870+SagarKharat94440@users.noreply.github.com>
2025-07-19 00:52:37 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav">
       <div>
         
-        <h1 className="text-5xl font-signature ml-2"><a className="link-underline link-underline-black" target="_blank" rel="noreferrer">Sagar Kharat</a></h1>
+        <h1 className="text-5xl font-signature ml-2"><a  href="https://sagarkharat94440.github.io/Portfolio/"  className="link-underline link-underline-black" target="_blank" rel="noreferrer">Sagar Kharat</a></h1>
       </div>
 
       <ul className="hidden md:flex">
